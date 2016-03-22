e7b6098c6170abdf3630f5241b777e61b8bfbf6b
Sal Espinosa <sespinos@gmail.com>
2016-03-22 08:13:48 -0600

    More challenges

---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+
+class NotNerdyApp < Sinatra::Base
+
+  get "/" do
+    "Would you like to play a game?"
+  end
+
+  post "/" do
+    if params[:answer] == "yes"
+      redirect "/something", 307
+    else
+      "You should give us your 'answer'"
+    end
+  end
+
+  get "/something" do
+    "Now show me how to 'put' your 'name' down"
+  end
+
+  put "/something" do
+    if params[:name]
+      redirect "/next_thing", 307
+    else
+      ""
+    end
+  end
+
+  get "/next_thing" do
+    "Come far you have... 'patch' the 'force'"
+  end
+
+  patch "/force" do
+    if params[:force]
+      redirect "/meteor"
+    end
+  end
+
+  get "/meteor" do
+    "A meteor is coming crashing to the earth... what should you do..."
+  end
+
+  delete "/meteor" do
+    "You win - Thanks for playing!"
+  end
+
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+require_relative 'app'
+require 'shotgun'
+
+run NotNerdyApp
